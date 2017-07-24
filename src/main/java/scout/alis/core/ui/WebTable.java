8e682650a793eb846a93b9acc276c6e08f747642
amid.k <amid.keadan@gmail.com>
2017-07-24 18:11:01 +0300

    upload test data excel file

--- src/main/java/scout/alis/core/ui/WebTable.java
+++ src/main/java/scout/alis/core/ui/WebTable.java
@@ -1,17 +1,19 @@
 package scout.alis.core.ui;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import webdriver.Driver;
+import webdriver.FindWebElements;
 
 public class WebTable {
 	
 	protected final static WebDriver DRIVER = Driver.instance;
-	private static JavascriptExecutor js =(JavascriptExecutor)DRIVER;
-	private static WebElement element = null;
-	private static String fullXpath = null;
+	private WebElement TableObject= null ;
 	
 	private String TXT_Caption="";
 	private String TXT_id;
@@ -19,16 +21,32 @@ public class WebTable {
 
 	public WebTable(String txt_Caption) {
 		this.TXT_Caption = txt_Caption;
+		this.TableObject = findTableByCaption(txt_Caption);
 	}
 	
-/*	public String[] getTableHeaders(){
+	public String[] getTableHeaders(){
 		
-		return ;
+		 String[]headerArray = null;
+        List <WebElement> headeritems =  this.TableObject.findElements(By.className("v-table-caption-container"));
+        
+        headerArray = new String[headeritems.size()];
+        int i =0;
+        for ( WebElement item: headeritems) {
+        	System.out.println(item.getText());
+        	headerArray[i] =item.getText();
+        	i++;
+        }
+        
+		return headerArray;
 	}
 	
 	
 	private WebElement findTableByCaption(String sCaption)
 	{
-		String fullXpath = String.format("//div[div[text()='%s']]", sTable);	
-	}*/
+		String fullXpath = String.format("//div[div[text()='%s']]", sCaption);
+		WebElement tableCaption = FindWebElements.findWebElementVisibleByXpath(fullXpath);
+		WebElement nextsibling = tableCaption.findElement(By.xpath("following-sibling::div"));
+		
+		return nextsibling;
+	}
 }
